639b4a79f1417ea2160572b9c35d38d79ac980a2
Gnanesh26 <gnaneswarrao.yeddu@imaginnovate.com>
2023-11-14 19:36:03 +0530

    initial commit

---
+++ boot-cxf-soap-demo/src/main/java/com/briansdevblog/bootcxfsoapdemo/BootCxfSoapDemoApplication.java
@@ -0,0 +1,13 @@
+package com.briansdevblog.bootcxfsoapdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootCxfSoapDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootCxfSoapDemoApplication.class, args);
+	}
+
+}
---
+++ boot-cxf-soap-demo/src/test/java/com/briansdevblog/bootcxfsoapdemo/BootCxfSoapDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.briansdevblog.bootcxfsoapdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootCxfSoapDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
